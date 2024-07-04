5ca2ff3f36724f7c13e1d2cd7bdd30c01ac53e84
Therese Mills <tmills@Thereses-MacBook-Pro.local>
2024-07-04 10:17:08 -0400

    set plan

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# read in all satellites and propogate them in time to most recent one
+
+# determine length of time to care about and failures (time and satellite)
+
+# run model to service each failure, recording time it takes to service
+
+# compute mean and variance
