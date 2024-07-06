de591252d66b08968a25589dde5f7e7d2cadce97
Therese Mills <tmills@Thereses-MacBook-Pro.local>
2024-07-06 12:02:59 -0400

    step 1 in main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+from parseTLE import *
+
 # read in all satellites and propogate them in time to most recent one
+filename = "./data/geoSmall.txt"
+sats = TLEdata(filename)
+[(newestYear, newestDay), (oldestYear, oldestYear)] = sats.getOldestAndNewest()
+sats.propogateToSameTime(newestYear, newestDay)
 
 # determine length of time to care about and failures (time and satellite)
 
--- src/parseTLE.py
+++ src/parseTLE.py
@@ -1,79 +1,89 @@
 import math
 
-def parse(filename):
-
-    # Read TLE file
-    with open(filename, "r") as f:
-        tle_lines = f.readlines()
-
-    all_satellites = dict()
-    counter = 1
-    for i in range(len(tle_lines)):
-        if i%3 == 0:
-            all_satellites[counter] = {"name" : tle_lines[i].strip()}
+class TLEdata:
+    def __init__(self, filename):
+        self.filename = filename
+        self.satellites = self.parse()
         
-        if i%3 == 1:
-            year = int(tle_lines[i][18:20]) + 1900
-            if year < 1957:
-                year += 100
-            all_satellites[counter]["epochYear"] = year # year
-            all_satellites[counter]["epochDay"] = float(tle_lines[i][20:32]) # day of the year and fractional portion of the day
+    def parse(self):
 
-        if i%3 == 2:
-            all_satellites[counter]["inclination"] = math.radians(float(tle_lines[i][8:16])) # radians
-            all_satellites[counter]["Omega"] = math.radians(float(tle_lines[i][17:25])) # radians
-            all_satellites[counter]["eccentricity"] = float(tle_lines[i][26:33]) * 1e-7 
-            all_satellites[counter]["omega"] = math.radians(float(tle_lines[i][34:42])) # radians
-            all_satellites[counter]["M"] = math.radians(float(tle_lines[i][43:51])) # radians
-            all_satellites[counter]["n"] = float(tle_lines[i][52:63]) # revs/day
-            counter += 1
+        # Read TLE file
+        with open(self.filename, "r") as f:
+            tle_lines = f.readlines()
 
-    return all_satellites
+        all_satellites = dict()
+        counter = 1
+        for i in range(len(tle_lines)):
+            if i%3 == 0:
+                all_satellites[counter] = {"name" : tle_lines[i].strip()}
+            
+            if i%3 == 1:
+                year = int(tle_lines[i][18:20]) + 1900
+                if year < 1957:
+                    year += 100
+                all_satellites[counter]["epochYear"] = year # year
+                all_satellites[counter]["epochDay"] = float(tle_lines[i][20:32]) # day of the year and fractional portion of the day
 
-def getOldestAndNewest(satellites):
-    oldestYear = None
-    oldestDay = None
-    newestYear = None
-    newestDay = None
+            if i%3 == 2:
+                all_satellites[counter]["inclination"] = math.radians(float(tle_lines[i][8:16])) # radians
+                all_satellites[counter]["Omega"] = math.radians(float(tle_lines[i][17:25])) # Right Ascension of Ascending Node (RAAN), radians
+                all_satellites[counter]["eccentricity"] = float(tle_lines[i][26:33]) * 1e-7 
+                all_satellites[counter]["omega"] = math.radians(float(tle_lines[i][34:42])) # Argument of Perigee, radians
+                all_satellites[counter]["M"] = math.radians(float(tle_lines[i][43:51])) # Mean Anomaly, radians
+                all_satellites[counter]["n"] = float(tle_lines[i][52:63]) # Mean Motion, revs/day
+                counter += 1
 
-    for sat in satellites.values():
-        candidateYear = sat['epochYear']
-        candidateDay = sat['epochDay']
+        return all_satellites
 
-        if oldestYear is None or candidateYear <= oldestYear:
-            if candidateYear == oldestYear:
-                if candidateDay <= oldestDay:
+    def getOldestAndNewest(self):
+        oldestYear = None
+        oldestDay = None
+        newestYear = None
+        newestDay = None
+
+        for sat in self.satellites.values():
+            candidateYear = sat['epochYear']
+            candidateDay = sat['epochDay']
+
+            if oldestYear is None or candidateYear <= oldestYear:
+                if candidateYear == oldestYear:
+                    if candidateDay <= oldestDay:
+                        oldestYear = candidateYear
+                        oldestDay = candidateDay
+                else:
                     oldestYear = candidateYear
                     oldestDay = candidateDay
-            else:
-                oldestYear = candidateYear
-                oldestDay = candidateDay
 
-        if newestYear is None or candidateYear >= newestYear:
-            if candidateYear == newestYear:
-                if candidateDay >= newestDay:
+            if newestYear is None or candidateYear >= newestYear:
+                if candidateYear == newestYear:
+                    if candidateDay >= newestDay:
+                        newestYear = candidateYear
+                        newestDay = candidateDay
+                else:
                     newestYear = candidateYear
                     newestDay = candidateDay
-            else:
-                newestYear = candidateYear
-                newestDay = candidateDay
 
-    return [(newestYear, newestDay), (oldestYear, oldestYear)]
+        return [(newestYear, newestDay), (oldestYear, oldestYear)]
 
-def propogateToSameTime(satellites, goalYear, goalDay):
+    def propogateToSameTime(self, goalYear, goalDay):
 
-    # TODO this assumes all the same year, which is true for geo.txt
-    for sat in satellites.values():
-        curYear = sat['epochYear']
-        curDay = sat['epochDay']
+        # TODO this assumes all the same year, which is true for geo.txt
+        for sat in self.satellites.values():
+            curYear = sat['epochYear']
+            curDay = sat['epochDay']
 
-        daysToProp = goalDay - curDay
-        secsToProp = daysToProp * 24 * 60 * 60
+            # get time needed to propogate
+            daysToProp = goalDay - curDay
+            secsToProp = daysToProp * 24 * 60 * 60
 
-
+            # update the epoch and mean anomaly
+            sat['epochYear'] = goalYear
+            sat['epochDay'] = goalDay
+            sat['M'] += sat['n'] * 2 * math.pi * daysToProp
 
 if __name__ == "__main__":
     filename = "./data/geoSmall.txt"
-    sats = parse(filename)
-    [(newestYear, newestDay), (oldestYear, oldestYear)] = getOldestAndNewest(sats)
-    print(propogateToSameTime(sats, newestYear, newestDay))
+    sats = TLEdata(filename)
+    [(newestYear, newestDay), (oldestYear, oldestYear)] = sats.getOldestAndNewest()
+    sats.propogateToSameTime(newestYear, newestDay)
+    print(sats.satellites[1])
